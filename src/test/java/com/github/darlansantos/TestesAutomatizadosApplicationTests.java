fbd914f488a89fe3232aa1f021260a9f8b2e928c
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2020-10-04 00:54:48 -0300

    first commit

---
+++ src/test/java/com/github/darlansantos/TestesAutomatizadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.darlansantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesAutomatizadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
